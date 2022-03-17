a45623f170a7927727bfe318f622119f5d596385
Nilesh Athghara <nileshk892@gmail.com>
2022-03-17 13:28:34 +0530

    academy

---
+++ academy/script.js
@@ -0,0 +1,23 @@
+function CloseSession() {
+    app.CloseSession();
+}
+
+function processDone() {
+    app.processDone();
+}
+
+
+function SuccessTraining(param1, param2, param3,param4) {
+    app.SuccessTraining(param1, param2, param3,param4);
+}
+
+
+function clearWebBrowser() {
+    app.clearWebBrowser();
+}
+
+function skipBBps() {
+    app.skipBBps();
+}
+
+
