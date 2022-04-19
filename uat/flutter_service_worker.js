a669aeb7e9cff274d58a6e9030fdb03d613118a7
Nilesh Athghara <nileshk892@gmail.com>
2022-04-19 09:42:08 +0530

    address fixed

--- uat/flutter_service_worker.js
+++ uat/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6addbc5d57fd3a1bf9c5b2f738ef00b9",
-"index.html": "3a8920cd18c50873056c7ce23ffa3a17",
-"/": "3a8920cd18c50873056c7ce23ffa3a17",
+"index.html": "19bfc89777d5bb7e5656d0696b62f285",
+"/": "19bfc89777d5bb7e5656d0696b62f285",
 "firebase-messaging-sw.js": "e3cb633ced387d5a461ec4350cb85275",
-"main.dart.js": "08d04aa18588b77c3817c05267de4634",
+"main.dart.js": "8ada7b900b3a146c98483fd3fddb19b5",
 "ic_launcher.png": "b56c0391abbb75a4e7050e040abfbd0c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/ic_launcher.png": "38a88cd6e2a00dcf72456a689c4d9b66",
