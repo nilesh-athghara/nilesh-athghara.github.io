c35814265835148186d5aca0d1ff705cf3ced68d
Nilesh Athghara <nileshk892@gmail.com>
2022-06-30 11:35:47 +0530

    qr prod api

--- qr/flutter_service_worker.js
+++ qr/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6addbc5d57fd3a1bf9c5b2f738ef00b9",
-"index.html": "a9a2b2a3c67a618a0bfc11bf3fb24a63",
-"/": "a9a2b2a3c67a618a0bfc11bf3fb24a63",
+"index.html": "e6ea99029f80b859d463483318c49a63",
+"/": "e6ea99029f80b859d463483318c49a63",
 "firebase-messaging-sw.js": "e3cb633ced387d5a461ec4350cb85275",
-"main.dart.js": "3c96b949fcb168c84f27327691bc849a",
+"main.dart.js": "7e2d403b8d6c8314a995b680887f625d",
 "ic_launcher.png": "b56c0391abbb75a4e7050e040abfbd0c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/ic_launcher.png": "38a88cd6e2a00dcf72456a689c4d9b66",
